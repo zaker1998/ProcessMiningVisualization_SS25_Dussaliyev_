348cb27d8911e8b99df0dcb0a0d0295d0e24389c
fabianf00 <fabian.frauenberger@me.com>
2024-03-03 11:02:39 +0100

    add streamlit-component template for interactive graphs

---
+++ components/interactiveGraph/frontend/src/InteractiveGraph.tsx
@@ -0,0 +1,49 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+} from "streamlit-component-lib"
+import React, { ReactNode } from "react"
+
+interface State {
+  numClicks: number
+  isFocused: boolean
+}
+
+class InteractiveGraph extends StreamlitComponentBase<State> {
+
+  public render = (): ReactNode => {
+    // Arguments that are passed to the plugin in Python are accessible
+    // via `this.props.args`. Pass the new value back to
+    // Streamlit via `Streamlit.setComponentValue`.
+
+    // Streamlit sends us a theme object via props that we can use to ensure
+    // that our component has visuals that match the active theme in a
+    // streamlit app.
+    const { theme } = this.props
+    const style: React.CSSProperties = {}
+
+    // Maintain compatibility with older versions of Streamlit that don't send
+    // a theme object.
+    if (theme) {
+      // Use the theme object to style our button border. Alternatively, the
+      // theme style is defined in CSS vars.
+      const borderStyling = `1px solid ${
+        this.state.isFocused ? theme.primaryColor : "gray"
+      }`
+      style.border = borderStyling
+      style.outline = borderStyling
+    }
+
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+// "withStreamlitConnection" is a wrapper function. It bootstraps the
+// connection between your component and the Streamlit app, and handles
+// passing arguments from Python -> Component.
+//
+// You don't need to edit withStreamlitConnection (but you're welcome to!).
+export default withStreamlitConnection(InteractiveGraph)
---
+++ components/interactiveGraph/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import InteractiveGraph from "./InteractiveGraph"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <InteractiveGraph />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
