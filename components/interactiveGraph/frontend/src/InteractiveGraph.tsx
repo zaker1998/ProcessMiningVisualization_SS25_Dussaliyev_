cd833d8cfeb8574ea1d7877c034d81b1502914d6
fabianf00 <fabian.frauenberger@me.com>
2024-03-03 13:04:47 +0100

    update component template

--- components/interactiveGraph/frontend/src/InteractiveGraph.tsx
+++ components/interactiveGraph/frontend/src/InteractiveGraph.tsx
@@ -1,49 +1,17 @@
 import {
   Streamlit,
-  StreamlitComponentBase,
   withStreamlitConnection,
+  ComponentProps,
 } from "streamlit-component-lib"
-import React, { ReactNode } from "react"
+import React, { useEffect } from "react"
 
-interface State {
-  numClicks: number
-  isFocused: boolean
+const InteractiveGraph: React.FC<ComponentProps> = (props) => {
+  const { args } = props
+  useEffect(() => {
+    Streamlit.setFrameHeight(600)
+  }, [])
+
+  return <div>Interactive Graph</div>
 }
 
-class InteractiveGraph extends StreamlitComponentBase<State> {
-
-  public render = (): ReactNode => {
-    // Arguments that are passed to the plugin in Python are accessible
-    // via `this.props.args`. Pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
-
-    // Streamlit sends us a theme object via props that we can use to ensure
-    // that our component has visuals that match the active theme in a
-    // streamlit app.
-    const { theme } = this.props
-    const style: React.CSSProperties = {}
-
-    // Maintain compatibility with older versions of Streamlit that don't send
-    // a theme object.
-    if (theme) {
-      // Use the theme object to style our button border. Alternatively, the
-      // theme style is defined in CSS vars.
-      const borderStyling = `1px solid ${
-        this.state.isFocused ? theme.primaryColor : "gray"
-      }`
-      style.border = borderStyling
-      style.outline = borderStyling
-    }
-
-    return (
-      <h1>Hello World</h1>
-    )
-  }
-}
-
-// "withStreamlitConnection" is a wrapper function. It bootstraps the
-// connection between your component and the Streamlit app, and handles
-// passing arguments from Python -> Component.
-//
-// You don't need to edit withStreamlitConnection (but you're welcome to!).
 export default withStreamlitConnection(InteractiveGraph)
