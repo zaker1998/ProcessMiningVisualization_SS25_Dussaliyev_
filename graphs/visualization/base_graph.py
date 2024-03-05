0be5f4708cf054827902d923262772ec19696d87
fabianf00 <fabian.frauenberger@me.com>
2024-03-05 20:25:46 +0100

    add graphviz graph as class variable

--- graphs/visualization/base_graph.py
+++ graphs/visualization/base_graph.py
@@ -7,7 +7,6 @@ class Node:
         id: str | int,
         label: str = "",
         data: dict[str, str | int] = None,
-        node_attributes: dict[str, str] = None,
     ) -> None:
         self.id = id
         if label:
@@ -15,7 +14,6 @@ class Node:
         else:
             self.label = str(id)
         self._data = data
-        self._node_attributes = node_attributes
 
     def get_label(self) -> str:
         return self.label
@@ -31,9 +29,6 @@ class Node:
             return None
         return self._data[key]
 
-    def get_node_attributes(self) -> dict[str, str]:
-        return self._node_attributes
-
 
 class Edge:
     def __init__(
@@ -41,12 +36,10 @@ class Edge:
         source: str | int,
         destination: str | int,
         weight: int = 1,
-        edge_attributes: dict[str, str] = None,
     ) -> None:
         self.source = source
         self.destination = destination
         self.weight = weight
-        self.edge_attributes = edge_attributes
 
     def get_edge(self) -> tuple[str | int, str | int, int]:
         return (self.source, self.destination, self.weight)
@@ -56,14 +49,26 @@ class BaseGraph:
     def __init__(
         self,
         directed: bool = True,
+        graph_attributes: dict[str, str] = None,
         global_node_attributes: dict[str, str] = None,
         global_edge_attributes: dict[str, str] = None,
     ) -> None:
         self.directed = directed
         self.nodes = {}
         self.edges = {}
-        self._global_node_attributes = global_node_attributes
-        self._global_edge_attributes = global_edge_attributes
+
+        __graph_attributes = graph_attributes or {}
+        __global_node_attributes = global_node_attributes or {}
+        __global_edge_attributes = global_edge_attributes or {}
+
+        self.graph = (
+            graphviz.Digraph(**graph_attributes)
+            if directed
+            else graphviz.Graph(**graph_attributes)
+        )
+
+        self.graph.attr("node", **__global_node_attributes)
+        self.graph.attr("edge", **__global_edge_attributes)
 
     def add_node(
         self,
@@ -77,6 +82,10 @@ class BaseGraph:
             raise ValueError(f"Node with id {id} already exists.")
         node = Node(id, label, data, node_attributes)
         self.nodes[node.get_id()] = node
+
+        __node_attributes = node_attributes or {}
+
+        self.graph.node(str(id), node.get_label(), **__node_attributes)
 
     def add_edge(
         self,
@@ -93,6 +102,14 @@ class BaseGraph:
 
         edge = Edge(source_id, target_id, weight, edge_attributes)
         self.edges[(edge.source, edge.destination)] = edge
+
+        __edge_attributes = edge_attributes or {}
+        self.graph.edge(
+            str(edge.source),
+            str(edge.destination),
+            label=str(edge.weight),
+            **__edge_attributes,
+        )
 
     def get_node(self, id: str | int) -> Node:
         if not self.contains_node(id):
@@ -126,30 +143,4 @@ class BaseGraph:
         return list(self.edges.values())
 
     def get_graphviz_string(self) -> str:
-        if self.directed:
-            graph = graphviz.Digraph()
-        else:
-            graph = graphviz.Graph()
-
-        if self._global_node_attributes:
-            graph.attr(**self._global_node_attributes)
-
-        if self._global_edge_attributes:
-            graph.attr(**self._global_edge_attributes)
-
-        for node in self.get_nodes():
-            node_attributes = node.get_node_attributes() or {}
-            graph.node(
-                str(node.get_id()),
-                label=node.get_label(),
-                **node_attributes,
-            )
-        for edge in self.get_edges():
-            edge_attributes = edge.edge_attributes or {}
-            graph.edge(
-                str(edge.source),
-                str(edge.destination),
-                label=str(edge.weight),
-                **edge_attributes,
-            )
-        return graph.source
+        return self.graph.source
