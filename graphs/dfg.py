194ea117bf41250a1525dd0e03c3c36d5061fdfc
fabianf00 <fabian.frauenberger@me.com>
2024-03-08 15:29:56 +0100

    implement directly follows graph

--- graphs/dfg.py
+++ graphs/dfg.py
@@ -2,13 +2,22 @@ class DFG:
     """Implementation of a Directly Follows Graph (DFG)"""
 
     def __init__(self, log: list[list[str]]) -> None:
+        self.start_nodes: set[str | int] = set()
+        self.end_nodes: set[str | int] = set()
         self.nodes: set[str | int] = set()
         self.edges: dict[tuple[str | int, str | int], int] = dict()
-        self.build_graph_from_log(log)
+        self.__build_graph_from_log(log)
 
-    def _build_graph_from_log(self, log: list[list[str]]) -> None:
-        # TODO: Implement this method
-        pass
+    def __build_graph_from_log(self, log: list[list[str]]) -> None:
+        for trace in log:
+            if len(trace) == 0:
+                continue
+
+            self.start_nodes.add(trace[0])
+            self.end_nodes.add(trace[-1])
+
+            for i in range(len(trace) - 1):
+                self.add_edge(trace[i], trace[i + 1])
 
     def add_edge(
         self, source: str | int, destination: str | int, weight: int = 1
@@ -32,3 +41,21 @@ class DFG:
 
     def get_edges(self) -> dict[tuple[str | int, str | int], int]:
         return self.edges
+
+    def get_start_nodes(self) -> set[str | int]:
+        return self.start_nodes
+
+    def get_end_nodes(self) -> set[str | int]:
+        return self.end_nodes
+
+    def is_in_start_nodes(self, node: str | int) -> bool:
+        return node in self.start_nodes
+
+    def is_in_end_nodes(self, node: str | int) -> bool:
+        return node in self.end_nodes
+
+    def contains_node(self, node: str | int) -> bool:
+        return node in self.nodes
+
+    def contains_edge(self, source: str | int, destination: str | int) -> bool:
+        return (source, destination) in self.edges
