011d0049278d7923dd148174b45017cd6982de8f
fabianf00 <fabian.frauenberger@me.com>
2024-03-15 14:11:37 +0100

    use own graph class for the heuristic miner

--- custom_ui/heuristic_graph_ui/heuristic_graph_view.py
+++ custom_ui/heuristic_graph_ui/heuristic_graph_view.py
@@ -1,30 +1,42 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QFileDialog, QWidget, QLabel,QVBoxLayout, QHBoxLayout, QFrame
+from PyQt5.QtWidgets import (
+    QFileDialog,
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QHBoxLayout,
+    QFrame,
+)
 from api.custom_error import FileNotFoundException
-from custom_ui.heuristic_graph_ui.heuristic_graph_controller import HeuristicGraphController
+from custom_ui.heuristic_graph_ui.heuristic_graph_controller import (
+    HeuristicGraphController,
+)
 from custom_ui.algorithm_view_interface import AlgorithmViewInterface
 from custom_ui.d3_html_widget import HTMLWidget
 from custom_ui.custom_widgets import SaveProjectButton, ExportButton, CustomQSlider
 
+
 class HeuristicGraphView(QWidget, AlgorithmViewInterface):
-    def __init__(self, parent, saveFolder = "saves/", workingDirectory = 'temp/graph_viz'):
+    def __init__(self, parent, saveFolder="saves/", workingDirectory="temp/graph_viz"):
         super().__init__()
         self.parent = parent
         self.initialized = False
 
-        #modifiers and global variables
+        # modifiers and global variables
         self.default_dependency_threshold = 0.5
         self.dependency_threshold = self.default_dependency_threshold
         self.default_min_frequency = 1
         self.min_frequency = self.default_min_frequency
         self.max_frequency = 100
         self.saveFolder = saveFolder
-        self.workingDirectory = workingDirectory # the working directory is where the graphviz file is stored for display and export
-        self.HeuristicGraphController = HeuristicGraphController(workingDirectory, self.dependency_threshold, self.min_frequency) 
-        self.graphviz_graph = None # the graphviz object
+        self.workingDirectory = workingDirectory  # the working directory is where the graphviz file is stored for display and export
+        self.HeuristicGraphController = HeuristicGraphController(
+            workingDirectory, self.dependency_threshold, self.min_frequency
+        )
+        self.graph = None  # the graph object
 
         self.graph_widget = HTMLWidget(parent)
-        
+
         # Create the slider frame
         slider_frame = QFrame()
         slider_frame.setFrameShape(QFrame.StyledPanel)
@@ -44,10 +56,14 @@ class HeuristicGraphView(QWidget, AlgorithmViewInterface):
         slider_layout.addWidget(self.thresh_slider)
         slider_layout.addWidget(self.thresh_slider)
 
-        self.saveProject_button = SaveProjectButton(self.parent,self.saveFolder,self.getModel)
+        self.saveProject_button = SaveProjectButton(
+            self.parent, self.saveFolder, self.getModel
+        )
         self.export_button = ExportButton(self.parent)
         slider_frame_layout = QVBoxLayout()
-        slider_frame_layout.addWidget(QLabel("Heuristic Mining Modifiers", alignment=Qt.AlignCenter))
+        slider_frame_layout.addWidget(
+            QLabel("Heuristic Mining Modifiers", alignment=Qt.AlignCenter)
+        )
         slider_frame_layout.addLayout(slider_layout)
         slider_frame_layout.addWidget(self.saveProject_button)
         slider_frame_layout.addWidget(self.export_button)
@@ -60,26 +76,28 @@ class HeuristicGraphView(QWidget, AlgorithmViewInterface):
 
         self.setLayout(main_layout)
 
-    # CALL BEFORE USAGE (option 1 for mining new models) 
+    # CALL BEFORE USAGE (option 1 for mining new models)
     def startMining(self, filename, cases):
 
         self.saveProject_button.load_filename(filename)
         self.HeuristicGraphController.startMining(cases)
-        
+
         self.min_frequency = self.default_min_frequency
         self.dependency_threshold = self.default_dependency_threshold
         self.max_frequency = self.HeuristicGraphController.get_max_frequency()
-        self.freq_slider.setRange(1,self.max_frequency)
-        self.__set_slider_values(self.min_frequency,self.dependency_threshold)
+        self.freq_slider.setRange(1, self.max_frequency)
+        self.__set_slider_values(self.min_frequency, self.dependency_threshold)
 
         self.graph_widget.start_server()
-        self.initialized=True
+        self.initialized = True
         self.__mine_and_draw()
 
     # CALL BEFORE USAGE (option 2 for mining existing models)
     def loadModel(self):
         try:
-            file_path, _ = QFileDialog.getOpenFileName(None, "Select file", self.saveFolder, "Pickle files (*.pickle)")
+            file_path, _ = QFileDialog.getOpenFileName(
+                None, "Select file", self.saveFolder, "Pickle files (*.pickle)"
+            )
             # If the user cancels the file dialog, return
             if not file_path:
                 return -1
@@ -91,16 +109,16 @@ class HeuristicGraphView(QWidget, AlgorithmViewInterface):
             print(str(e))
             self.parent.show_pop_up_message(message, 6000)
             return -1
-        
+
         self.saveProject_button.load_filename(filename)
 
         self.max_frequency = self.HeuristicGraphController.get_max_frequency()
-        self.freq_slider.setRange(1,self.max_frequency)
+        self.freq_slider.setRange(1, self.max_frequency)
         self.min_frequency = self.HeuristicGraphController.get_min_frequency()
         self.dependency_threshold = self.HeuristicGraphController.get_threshold()
-        
-        self.__set_slider_values(self.min_frequency,self.dependency_threshold)
-        
+
+        self.__set_slider_values(self.min_frequency, self.dependency_threshold)
+
         self.graph_widget.start_server()
         self.initialized = True
         self.__mine_and_draw()
@@ -108,7 +126,7 @@ class HeuristicGraphView(QWidget, AlgorithmViewInterface):
     # this function is given to the Save Project button. It is called whenever we save the model.
     def getModel(self):
         return self.HeuristicGraphController.getModel()
-    
+
     def __freq_slider_changed(self, value):
 
         # Update the label with the slider value
@@ -116,70 +134,70 @@ class HeuristicGraphView(QWidget, AlgorithmViewInterface):
 
         if not self.initialized:
             return
-        
+
         # Redraw graph when value changes
         self.min_frequency = value
         self.__mine_and_draw()
-    
+
     def __thresh_slider_changed(self, value):
         # Update the label with the slider value
         self.thresh_slider.setText(f"Dependency Threshold: {value/100:.2f}")
 
         if not self.initialized:
             return
-        
+
         # Redraw graph when value changes
-        self.dependency_threshold = value/100
-    
+        self.dependency_threshold = value / 100
+
         self.__mine_and_draw()
 
     def __set_slider_values(self, min_freq, threshold):
         self.thresh_slider.setText(f"Dependency Threshold: {threshold:.2f}")
         self.freq_slider.setText(f"Min. Frequency: {min_freq}")
-        self.thresh_slider.setValue(int(threshold*100))
+        self.thresh_slider.setValue(int(threshold * 100))
         self.freq_slider.setValue(min_freq)
 
     def __mine_and_draw(self):
-
-        '''with graphviz'''
-        self.graphviz_graph = self.HeuristicGraphController.create_dependency_graph(self.dependency_threshold,self.min_frequency)
+        self.graph = self.HeuristicGraphController.create_dependency_graph(
+            self.dependency_threshold, self.min_frequency
+        )
 
         # Load the image
-        filename = self.workingDirectory + '.dot'
+        filename = self.workingDirectory + ".dot"
         self.graph_widget.set_source(filename)
         try:
             self.graph_widget.reload()
         except FileNotFoundException as e:
             print(e.message)
 
-    def __ensure_graphviz_graph_exists(self):
+    def __ensure_graph_exists(self):
         # just to make sure everything works as intended.
-        if not self.graphviz_graph:
-            print("HeuristicGraphView ERROR: graphviz_graph is NONE.")
+        if not self.graph:
+            print("HeuristicGraphView ERROR: graph is NONE.")
             return False
         return True
 
     def generate_png(self):
-        if not self.__ensure_graphviz_graph_exists():
+        if not self.__ensure_graph_exists():
             return
-        self.graphviz_graph.render(self.workingDirectory,format = 'png')
+        self.graph.export_graph(self.workingDirectory, format="png")
         print("heuristic_graph_view: PNG generated")
         return
 
     def generate_svg(self):
-        if not self.__ensure_graphviz_graph_exists():
+        if not self.__ensure_graph_exists():
             return
-        self.graphviz_graph.render(self.workingDirectory,format = 'svg')
+        self.graph.export_graph(self.workingDirectory, format="svg")
         print("heuristic_graph_view: SVG generated")
 
     def generate_dot(self):
-        if not self.__ensure_graphviz_graph_exists():
+        if not self.__ensure_graph_exists():
             return
-        self.graphviz_graph.render(self.workingDirectory,format = 'dot')
+        self.graph.export_graph(self.workingDirectory, format="dot")
         print("heuristic_graph_view: DOT generated")
 
     def clear(self):
         self.graph_widget.clear()
-        self.dependency_threshold= 0.5
+        self.dependency_threshold = 0.5
         self.min_frequency = 1
         self.zoom_factor = 1.0
--- mining_algorithms/heuristic_mining.py
+++ mining_algorithms/heuristic_mining.py
@@ -1,8 +1,9 @@
-from graphviz import Digraph
 import numpy as np
 from mining_algorithms.ddcal_clustering import DensityDistributionClusterAlgorithm
+from graphs.visualization.heuristic_graph import HeuristicGraph
 
-class HeuristicMining():
+
+class HeuristicMining:
     def __init__(self, log):
         self.log = log
         self.events, self.appearence_frequency = self.__filter_out_all_events()
@@ -11,36 +12,44 @@ class HeuristicMining():
 
         # Graph modifiers
         self.min_edge_thickness = 1
-        #self.max_node_size = 10
+        # self.max_node_size = 10
         self.min_node_size = 1.5
         self.min_frequency = 1
         self.dependency_threshold = 0.5
-    
-    def create_dependency_graph_with_graphviz(self, dependency_threshold, min_frequency):
-        dependency_graph = self.__create_dependency_graph(dependency_threshold, min_frequency)
+
+    def create_dependency_graph_with_graphviz(
+        self, dependency_threshold, min_frequency
+    ):
+        dependency_graph = self.__create_dependency_graph(
+            dependency_threshold, min_frequency
+        )
         self.dependency_threshold = dependency_threshold
         self.min_frequency = min_frequency
 
         # create graph
-        graph = Digraph()
+        graph = HeuristicGraph()
         # cluster the node sizes based on frequency
-        cluster = DensityDistributionClusterAlgorithm(list(self.appearence_frequency.values()))
+        cluster = DensityDistributionClusterAlgorithm(
+            list(self.appearence_frequency.values())
+        )
         freq_sorted = list(cluster.sorted_data)
         freq_labels_sorted = list(cluster.labels_sorted_data)
 
         # add nodes to graph
         for node in self.events:
             node_freq = self.appearence_frequency.get(node)
-            w = freq_labels_sorted[freq_sorted.index(node_freq)]/2 + self.min_node_size
-            h = w/3
-            #graph.node(str(node), label = str(node)+"\n"+str(node_freq),width = str(w), height = str(h))
-            graph.node(str(node), label = str(node)+"\n"+str(node_freq),width = str(w), height = str(h), shape="box", style = "rounded")
+            w = (
+                freq_labels_sorted[freq_sorted.index(node_freq)] / 2
+                + self.min_node_size
+            )
+            h = w / 3
+            graph.add_event(node, node_freq, (w, h))
 
         # cluster the edge thickness sizes based on frequency
         edge_frequencies = self.dependency_matrix.flatten()
         edge_frequencies = edge_frequencies[edge_frequencies >= 0.0]
         edge_frequencies = np.unique(edge_frequencies)
-        #print(edge_frequencies)
+        # print(edge_frequencies)
         cluster = DensityDistributionClusterAlgorithm(edge_frequencies)
         freq_sorted = list(cluster.sorted_data)
         freq_labels_sorted = list(cluster.labels_sorted_data)
@@ -48,36 +57,43 @@ class HeuristicMining():
         # add edges to graph
         for i in range(len(self.events)):
             for j in range(len(self.events)):
-                if dependency_graph[i][j] == 1.:
+                if dependency_graph[i][j] == 1.0:
                     if dependency_threshold == 0:
                         edge_thickness = 0.1
                     else:
-                        edge_thickness = freq_labels_sorted[freq_sorted.index(self.dependency_matrix[i][j])] + self.min_edge_thickness 
+                        edge_thickness = (
+                            freq_labels_sorted[
+                                freq_sorted.index(self.dependency_matrix[i][j])
+                            ]
+                            + self.min_edge_thickness
+                        )
+                    graph.add_edge(
+                        self.events[i],
+                        self.events[j],
+                        weight=int(self.succession_matrix[i][j]),
+                        size=edge_thickness,
+                    )
+        graph.add_start_node()
 
-                    graph.edge(str(self.events[i]), str(self.events[j]), penwidth = str(edge_thickness), label = str(int(self.succession_matrix[i][j])))
-
-        #add start node
-        graph.node("start", label = "start", shape='doublecircle', style='filled',fillcolor='green')
         for node in self.__get_start_nodes():
-            graph.edge("start", str(node), penwidth = str(0.1) )
+            graph.add_edge("start", node, size=0.1)
 
-        #add end node
-        graph.node("end", label = "end", shape='doublecircle', style='filled',fillcolor='red')
+        graph.add_end_node()
         for node in self.__get_end_nodes():
-            graph.edge(str(node), "end", penwidth =str( 0.1) )  
+            graph.add_edge(node, "end", size=0.1)
 
         return graph
-    
+
     def get_max_frequency(self):
         max_freq = 0
         for value in list(self.appearence_frequency.values()):
             if value > max_freq:
-                max_freq= value
+                max_freq = value
         return max_freq
-    
+
     def get_min_frequency(self):
         return self.min_frequency
-    
+
     def get_threshold(self):
         return self.dependency_threshold
 
@@ -86,7 +102,7 @@ class HeuristicMining():
         for trace in self.log:
             for activity in trace:
                 if activity in dic:
-                    dic[activity] = dic[activity]+1
+                    dic[activity] = dic[activity] + 1
                 else:
                     dic[activity] = 1
 
@@ -94,37 +110,37 @@ class HeuristicMining():
         return activities, dic
 
     def __create_succession_matrix(self):
-        succession_matrix = np.zeros((len(self.events),len(self.events)))
+        succession_matrix = np.zeros((len(self.events), len(self.events)))
         for trace in self.log:
             index_x = -1
             for element in trace:
-                
-                if index_x <0:
-                    index_x +=1
+
+                if index_x < 0:
+                    index_x += 1
                     continue
                 x = self.events.index(trace[index_x])
                 y = self.events.index(element)
-                succession_matrix[x][y]+=1
-                index_x +=1
+                succession_matrix[x][y] += 1
+                index_x += 1
         return succession_matrix
-    
+
     def __get_start_nodes(self):
-        #a start node is a node where an entire column in the succession_matrix is 0.
+        # a start node is a node where an entire column in the succession_matrix is 0.
         start_nodes = []
         for case in self.log:
             if case[0] not in start_nodes:
                 start_nodes.append(case[0])
-        
+
         return start_nodes
-        
+
     def __get_end_nodes(self):
-        #an end node is a node where an entire row in the succession_matrix is 0.
+        # an end node is a node where an entire row in the succession_matrix is 0.
         end_nodes = []
         for case in self.log:
-            last_index = len(case)-1
+            last_index = len(case) - 1
             if case[last_index] not in end_nodes:
                 end_nodes.append(case[last_index])
-        
+
         return end_nodes
 
     def __create_dependency_matrix(self):
@@ -134,11 +150,17 @@ class HeuristicMining():
             x = 0
             for i in row:
                 if x == y:
-                    dependency_matrix[x][y] = self.succession_matrix[x][y]/(self.succession_matrix[x][y]+1)
+                    dependency_matrix[x][y] = self.succession_matrix[x][y] / (
+                        self.succession_matrix[x][y] + 1
+                    )
                 else:
-                    dependency_matrix[x][y] = (self.succession_matrix[x][y]-self.succession_matrix[y][x])/(self.succession_matrix[x][y]+self.succession_matrix[y][x]+1)
-                x+=1
-            y+=1
+                    dependency_matrix[x][y] = (
+                        self.succession_matrix[x][y] - self.succession_matrix[y][x]
+                    ) / (
+                        self.succession_matrix[x][y] + self.succession_matrix[y][x] + 1
+                    )
+                x += 1
+            y += 1
         return dependency_matrix
 
     def __create_dependency_graph(self, dependency_treshhold, min_frequency):
@@ -146,8 +168,11 @@ class HeuristicMining():
         y = 0
         for row in dependency_graph:
             for x in range(len(row)):
-                if self.dependency_matrix[y][x] >= dependency_treshhold and self.succession_matrix[y][x] >= min_frequency:
-                    dependency_graph[y][x]+= 1          
-            y+=1
+                if (
+                    self.dependency_matrix[y][x] >= dependency_treshhold
+                    and self.succession_matrix[y][x] >= min_frequency
+                ):
+                    dependency_graph[y][x] += 1
+            y += 1
 
         return dependency_graph
