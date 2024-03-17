7dc839d51f2f6de5830e3c12577bb960b44a929b
fabianf00 <fabian.frauenberger@me.com>
2024-03-17 22:41:38 +0100

    Refactor heuristic testing and remove unused dependencies

--- tests/mining_algorithms/heuristic_mining_test.py
+++ tests/mining_algorithms/heuristic_mining_test.py
@@ -2,16 +2,14 @@
 This unittest tests the integrity of the heuristic_graph_controller which holds the HeuristicMining Model.
 """
 
-import pydot
 import unittest
 from custom_ui.heuristic_graph_ui.heuristic_graph_controller import (
     HeuristicGraphController,
 )
+from graphs.visualization.base_graph import BaseGraph
 from mining_algorithms.heuristic_mining import HeuristicMining
 from api.csv_preprocessor import read
-
-# I am using networkx here because GRAPHVIZ does not provide ANY GET-FUNCTIONS!
-import networkx as nx
+from collections import deque
 
 
 class TestHeuristic(unittest.TestCase):
@@ -67,21 +65,15 @@ class TestHeuristic(unittest.TestCase):
         Controller = HeuristicGraphController("temp/graph_viz")
         Controller.startMining(self.__read_cases("tests/testlogs/" + filename + ".txt"))
         G = Controller.create_dependency_graph(0.5, 1)
-        target = "temp/" + filename
-        dotsource = target + ".dot"
-        G.export_graph(target, format="dot")
-        self.__check_graph_integrity_with_netx(dotsource)
+        self.__check_graph_integrity(G)
 
     def __run_test_csv(self, threshold, min_freq):
         Controller = HeuristicGraphController("temp/graph_viz")
         Controller.startMining(read("tests/testcsv/test_csv.csv"))
         G = Controller.create_dependency_graph(
             threshold, min_freq
-        )  # G is a graphviz Digraph
-        target = "temp/test_csv"
-        dot_source = target + ".dot"
-        G.export_graph(target, format="dot")
-        self.__check_graph_integrity_with_netx(dot_source)
+        )  # G is a HeuricticGraph/BaseGraph
+        self.__check_graph_integrity(G)
 
     def __run_CallcenterExample_csv(self, threshold, min_freq):
         Controller = HeuristicGraphController("temp/graphviz")
@@ -95,11 +87,8 @@ class TestHeuristic(unittest.TestCase):
         )
         G = Controller.create_dependency_graph(
             threshold, min_freq
-        )  # G is a graphviz Digraph
-        target = "temp/callcenter"
-        dot_source = target + ".dot"
-        G.export_graph(target, format="dot")
-        self.__check_graph_integrity_with_netx(dot_source)
+        )  # G is a HeuricticGraph/BaseGraph
+        self.__check_graph_integrity(G)
 
     def __run_pickle_loading_test(self, pickleFile):
         Controller = HeuristicGraphController("temp/graph_viz")
@@ -119,53 +108,30 @@ class TestHeuristic(unittest.TestCase):
         threshold = Controller.get_threshold()
         self.assertIsNotNone(threshold)
 
-    def __check_graph_integrity_with_netx(self, dotsource):
-        # Read in dot file with NetworkX
-
-        # The commented code below uses Pygraphviz. Pydot is deprecated.
-        # But pygraphviz is a nightmare to install on Windows and requires a C/C++ compiler.
-        # This is something I can not require from any non professional user. (Not even from a professional developer)
-        # -- Anton Chen, June 2023
-        # nx_graph = nx.drawing.nx_agraph.read_dot(dotsource)
-
-        # This is code using pydot. A deprecated library without reliable replacement.
-        # So I will stick with it despite its deprecation status.
-        # Maybe someday PyGraphviz will be stable enough to replace it?
-        graph = pydot.graph_from_dot_file(dotsource)
-        nx_graph = nx.nx_pydot.from_pydot(graph[0])
+    def __check_graph_integrity(self, graph: BaseGraph):
+        self.assertTrue(graph.contains_node("Start"), "Start node not found.")
+        self.assertTrue(graph.contains_node("End"), "End node not found.")
 
         # Check there is a 'start' node that connects to at least 1 other node
-        start_node = None
-        for node in nx_graph.nodes:
-            if nx_graph.in_degree(node) == 0:  # In-degree of 0 indicates a 'start' node
-                start_node = node
-                break
+        self.assertTrue(
+            len(list(filter(lambda edge: edge.source == "Start", graph.get_edges())))
+            >= 1,
+            "Start node does not connect to any other nodes.",
+        )
 
-        assert start_node is not None, "No 'start' node found."
-        assert (
-            nx_graph.out_degree(start_node) >= 1
-        ), "The 'start' node does not connect to any other nodes."
+        # Check if all nodes are reachable from the 'start' node
+        reachable_nodes = set()
+        queue = deque(["Start"])
+        while queue:
+            node = queue.popleft()
+            reachable_nodes.add(node)
+            for edge in graph.get_edges():
+                if edge.source == node:
+                    if edge.destination not in reachable_nodes:
+                        queue.append(edge.destination)
 
-        # Check there is an 'end' node
-        end_node = None
-        for node in nx_graph.nodes:
-            if (
-                nx_graph.out_degree(node) == 0
-            ):  # Out-degree of 0 indicates an 'end' node
-                end_node = node
-                break
-
-        assert end_node is not None, "No 'end' node found."
-
-        # Check every node is reachable from some other node
-        reachable_nodes = nx.algorithms.dag.descendants(nx_graph, start_node)
-        reachable_nodes.add(start_node)
-
-        all_nodes = [
-            node
-            for node in nx_graph.nodes
-            if node.strip() != "\\n" and node.strip() != ""
-        ]
-        assert set(reachable_nodes) == set(
-            all_nodes
-        ), "Not all nodes are reachable from the 'start' node."
+        self.assertEqual(
+            reachable_nodes,
+            set(map(lambda node: node.id, graph.get_nodes())),
+            "Not all nodes are reachable from the 'start' node.",
+        )
