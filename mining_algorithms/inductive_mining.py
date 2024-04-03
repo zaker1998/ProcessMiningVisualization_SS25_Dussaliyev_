b78f745498c2b8f32bff9fe6fe9e633f06feb922
fabianf00 <fabian.frauenberger@me.com>
2024-04-03 21:14:27 +0200

    add base algorithm structure

--- mining_algorithms/inductive_mining.py
+++ mining_algorithms/inductive_mining.py
@@ -1,6 +1,33 @@
 from mining_algorithms.base_mining import BaseMining
+from utils.transformations import cases_list_to_dict
 
 
 class InductiveMining(BaseMining):
     def __init__(self, log):
         super().__init__(log)
+
+        if isinstance(log, list):
+            self.log = cases_list_to_dict(log)
+
+    def generate_graph(self):
+        pass
+
+    def inductive_mining(self, log):
+        if tree := self.base_cases(log):
+            return tree
+
+        if tuple() not in log:
+            if partitions := self.calulate_cut(log):
+                operation = partitions[0]
+                return (operation, *list(map(self.inductive_mining, partitions[1:])))
+
+        return self.fallthrough(log)
+
+    def base_cases(self, log):
+        pass
+
+    def calulate_cut(self, log):
+        pass
+
+    def fallthrough(self, log):
+        pass
