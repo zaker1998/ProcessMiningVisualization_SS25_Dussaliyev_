77525d9e042009ba2c99caee4c11f714c48aa659
fabianf00 <fabian.frauenberger@me.com>
2024-04-06 20:51:27 +0200

    implement common functionality in base class

--- mining_algorithms/base_mining.py
+++ mining_algorithms/base_mining.py
@@ -1,7 +1,60 @@
+from mining_algorithms.ddcal_clustering import DensityDistributionClusterAlgorithm
+
+
 class BaseMining:
     def __init__(self, log):
         self.log = log
+        self.min_node_size = 1.5
         self.graph = None
+        # self.events contains all events(unique!), appearance_activities are dictionaries, events:appearances ex. {'a':3, ...}
+        self.events, self.appearence_frequency = self.__filter_out_all_events()
+
+        """  
+        Info about DensityDistributionClusterAlgorithm DDCAL:
+         If we have a dictionary like dict={'a':123, 'c': 234, 'e': 345, 'b': 433, 'd': 456}
+         after using cluster.sorted_data we get a sorted array[123, 234, 345, 433, 456]
+         after using cluster.labels_sorted_data we get a normalized array with float numbers
+         minimum value starts with 0.0 and the greatest value has 5.0 in this case [0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
+        """
+        # cluster the node sizes based on frequency
+        cluster = DensityDistributionClusterAlgorithm(
+            list(self.appearence_frequency.values())
+        )
+        self.freq_sorted = list(cluster.sorted_data)
+        self.freq_labels_sorted = list(cluster.labels_sorted_data)
+
+        self.start_nodes = self.__get_start_nodes()
+        self.end_nodes = self.__get_end_nodes()
+
+    def __filter_out_all_events(self):
+        dic = {}
+        for trace in self.log:
+            for activity in trace:
+                if activity in dic:
+                    dic[activity] = dic[activity] + 1
+                else:
+                    dic[activity] = 1
+
+        activities = list(dic.keys())
+        return activities, dic
 
     def get_graph(self):
         return self.graph
+
+    def calulate_node_size(self, node):
+        scale_factor = self.get_scale_factor(node)
+
+        width = (scale_factor / 2) + self.min_node_size
+        height = width / 3
+        return width, height
+
+    def get_scale_factor(self, node):
+        node_freq = self.appearence_frequency.get(node)
+        scale_factor = self.freq_labels_sorted[self.freq_sorted.index(node_freq)]
+        return scale_factor
+
+    def __get_start_nodes(self):
+        return set([trace[0] for trace in self.log])
+
+    def __get_end_nodes(self):
+        return set([trace[-1] for trace in self.log])
--- mining_algorithms/heuristic_mining.py
+++ mining_algorithms/heuristic_mining.py
@@ -7,14 +7,11 @@ from mining_algorithms.base_mining import BaseMining
 class HeuristicMining(BaseMining):
     def __init__(self, log):
         super().__init__(log)
-        self.events, self.appearence_frequency = self.__filter_out_all_events()
         self.succession_matrix = self.__create_succession_matrix()
         self.dependency_matrix = self.__create_dependency_matrix()
 
         # Graph modifiers
         self.min_edge_thickness = 1
-        # self.max_node_size = 10
-        self.min_node_size = 1.5
         self.min_frequency = 1
         self.dependency_threshold = 0.5
 
@@ -30,20 +27,11 @@ class HeuristicMining(BaseMining):
         # create graph
         self.graph = HeuristicGraph()
         # cluster the node sizes based on frequency
-        cluster = DensityDistributionClusterAlgorithm(
-            list(self.appearence_frequency.values())
-        )
-        freq_sorted = list(cluster.sorted_data)
-        freq_labels_sorted = list(cluster.labels_sorted_data)
 
         # add nodes to graph
         for node in self.events:
             node_freq = self.appearence_frequency.get(node)
-            w = (
-                freq_labels_sorted[freq_sorted.index(node_freq)] / 2
-                + self.min_node_size
-            )
-            h = w / 3
+            w, h = self.calulate_node_size(node)
             self.graph.add_event(node, node_freq, (w, h))
 
         # cluster the edge thickness sizes based on frequency
@@ -51,6 +39,7 @@ class HeuristicMining(BaseMining):
         edge_frequencies = edge_frequencies[edge_frequencies >= 0.0]
         edge_frequencies = np.unique(edge_frequencies)
         # print(edge_frequencies)
+        # TODO: move in base class if used in other algorithms
         cluster = DensityDistributionClusterAlgorithm(edge_frequencies)
         freq_sorted = list(cluster.sorted_data)
         freq_labels_sorted = list(cluster.labels_sorted_data)
@@ -74,12 +63,17 @@ class HeuristicMining(BaseMining):
                         weight=int(self.succession_matrix[i][j]),
                         size=edge_thickness,
                     )
+        # add start and end nodes
         self.graph.add_start_node()
-
-        self.graph.add_starting_edges(self.__get_start_nodes())
-
         self.graph.add_end_node()
-        self.graph.add_ending_edges(self.__get_end_nodes())
+
+        # add starting and ending edges from the log
+        self.graph.add_starting_edges(self.start_nodes)
+        self.graph.add_ending_edges(self.end_nodes)
+
+        # TODO: add startign and ending edges for nodes that are
+        # 1. not reachable from the start node
+        # 2. not leading to the end node
 
     # TODO should be max frequency of edges and not nodes
     def get_max_frequency(self):
@@ -95,18 +89,6 @@ class HeuristicMining(BaseMining):
     def get_threshold(self):
         return self.dependency_threshold
 
-    def __filter_out_all_events(self):
-        dic = {}
-        for trace in self.log:
-            for activity in trace:
-                if activity in dic:
-                    dic[activity] = dic[activity] + 1
-                else:
-                    dic[activity] = 1
-
-        activities = list(dic.keys())
-        return activities, dic
-
     def __create_succession_matrix(self):
         succession_matrix = np.zeros((len(self.events), len(self.events)))
         for trace in self.log:
@@ -121,25 +103,6 @@ class HeuristicMining(BaseMining):
                 succession_matrix[x][y] += 1
                 index_x += 1
         return succession_matrix
-
-    def __get_start_nodes(self):
-        # a start node is a node where an entire column in the succession_matrix is 0.
-        start_nodes = []
-        for case in self.log:
-            if case[0] not in start_nodes:
-                start_nodes.append(case[0])
-
-        return start_nodes
-
-    def __get_end_nodes(self):
-        # an end node is a node where an entire row in the succession_matrix is 0.
-        end_nodes = []
-        for case in self.log:
-            last_index = len(case) - 1
-            if case[last_index] not in end_nodes:
-                end_nodes.append(case[last_index])
-
-        return end_nodes
 
     def __create_dependency_matrix(self):
         dependency_matrix = np.zeros(self.succession_matrix.shape)
