2a940c42028ea1c3b08435db2fd6624adb7fef20
fabianf00 <fabian.frauenberger@me.com>
2024-04-21 15:08:00 +0200

    make own button for exporting model, remove model from dropdown

--- views/ExportView.py
+++ views/ExportView.py
@@ -13,11 +13,33 @@ class ExportView(ViewInterface):
     def render(self):
         st.title("Export")
         graph = st.session_state.model.get_graph()
-        format = st.sidebar.selectbox("Export as:", ["SVG", "PNG", "DOT", "Model"])
-        if format.lower() == "model":
-            self.export_model(st.session_state.model)
-        else:
-            self.export_graph(graph, format.lower())
+        with st.sidebar:
+            format = st.selectbox("Export as:", ["SVG", "PNG", "DOT"])
+            format = format.lower()
+            self.dpi = st.number_input("DPI", min_value=50, value=96, step=1)
+            file_name = "temp/graph"
+            graph.export_graph(file_name, format, dpi=self.dpi)
+
+            export_button_col, model_export_button_col = st.columns([1, 1])
+
+            with export_button_col:
+                with open(file_name + "." + format, "rb") as file:
+                    st.download_button(
+                        label="Export",
+                        file_name="graph" + "." + format,
+                        data=file,
+                        mime="image/" + format if format != "dot" else "text/plain",
+                        type="primary",
+                    )
+
+            with model_export_button_col:
+                st.download_button(
+                    label="Export Model",
+                    file_name="model.pickle",
+                    data=pickle.dumps(st.session_state.model),
+                    mime="application/octet-stream",
+                    type="primary",
+                )
 
         start = time()
         graph.export_graph("temp/graph", "png", dpi=self.dpi)
@@ -35,25 +57,3 @@ class ExportView(ViewInterface):
             PNGViewer(png, height=600)
 
         navigation_button("Back", "Algorithm", type="secondary")
-
-    def export_graph(self, graph, format):
-        self.dpi = st.sidebar.number_input("DPI", min_value=50, value=96, step=1)
-
-        file_name = "temp/graph"
-        graph.export_graph(file_name, format, dpi=self.dpi)
-        with open(file_name + "." + format, "rb") as file:
-            st.sidebar.download_button(
-                label="Export",
-                file_name="graph" + "." + format,
-                data=file,
-                mime="image/" + format if format != "dot" else "text/plain",
-            )
-
-    def export_model(self, model):
-        filename = "temp/model.pickle"
-        st.sidebar.download_button(
-            label="Export",
-            file_name="model.pickle",
-            data=pickle.dumps(model),
-            mime="application/octet-stream",
-        )
