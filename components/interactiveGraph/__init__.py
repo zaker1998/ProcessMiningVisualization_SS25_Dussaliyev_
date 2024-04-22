51753b5336d4e24c84442fc3839fd69402f7a659
fabianf00 <fabian.frauenberger@me.com>
2024-04-22 13:31:57 +0200

    use dev component

--- components/interactiveGraph/__init__.py
+++ components/interactiveGraph/__init__.py
@@ -5,7 +5,7 @@ from graphs.visualization.base_graph import BaseGraph
 
 # Template for the component from https://docs.streamlit.io/library/components/publish and https://github.com/streamlit/component-template/tree/master/template/my_component
 
-_RELEASE = True
+_RELEASE = False
 _COMPONENT_NAME = "interactive-graph"
 
 if not _RELEASE:
