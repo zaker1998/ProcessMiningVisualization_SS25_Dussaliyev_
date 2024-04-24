86499bd2a06aba689a3dcf18311e84cf138d257d
fabianf00 <fabian.frauenberger@me.com>
2024-04-24 12:46:52 +0200

    add graph exception base class

--- exceptions/graph_exceptions.py
+++ exceptions/graph_exceptions.py
@@ -1,33 +1,39 @@
-class DuplicateNodeException(Exception):
+class DuplicateNodeException(GraphException):
 
     def __init__(self, node):
         self.message = f"Node '{node}' already exists in the graph."
         super().__init__(self.message)
 
 
-class DuplicateEdgeException(Exception):
+class DuplicateEdgeException(GraphException):
 
     def __init__(self, source, target):
         self.message = f"Edge from {source} to {target} already exists."
         super().__init__(self.message)
 
 
-class NodeDoesNotExistException(Exception):
+class NodeDoesNotExistException(GraphException):
 
     def __init__(self, node):
         self.message = f"Node '{node}' does not exist in the graph."
         super().__init__(self.message)
 
 
-class EdgeDoesNotExistException(Exception):
+class EdgeDoesNotExistException(GraphException):
 
     def __init__(self, source, target):
         self.message = f"Edge from {source} to {target} does not exist."
         super().__init__(self.message)
 
 
-class InvalidNodeNameException(Exception):
+class InvalidNodeNameException(GraphException):
 
     def __init__(self, node):
         self.message = f"Node '{node}' is not a valid node name."
+        super().__init__(self.message)
+
+
+def GraphException(Exception):
+    def __init__(self, message):
+        self.message = message
         super().__init__(self.message)
