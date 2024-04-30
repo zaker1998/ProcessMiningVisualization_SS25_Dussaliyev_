689e3180feabafc528e40f963a994e0bec2dfa8a
fabianf00 <fabian.frauenberger@me.com>
2024-04-30 13:23:50 +0200

    add updated heuristic miner ui

---
+++ ui/heuristic_miner_ui/heuristic_miner_controller.py
@@ -0,0 +1,48 @@
+from ui.base_algorithm_ui.base_algorithm_controller import BaseAlgorithmController
+from ui.heuristic_miner_ui.heuristic_miner_view import HeuristicMinerView
+import streamlit as st
+from mining_algorithms.heuristic_mining import HeuristicMining
+
+
+class HeuristicMinerController(BaseAlgorithmController):
+    def __init__(self, views=None, model=None):
+        if views is None:
+            views = [HeuristicMinerView()]
+        super().__init__(views, model)
+
+    def process_session_state(self):
+        super().process_session_state()
+
+        if "threshold" not in st.session_state:
+            st.session_state.threshold = self.mining_model.get_threshold()
+
+        if "frequency" not in st.session_state:
+            st.session_state.frequency = self.mining_model.get_min_frequency()
+
+        if "threshold" in st.session_state:
+            self.threshold = st.session_state.threshold
+
+        if "frequency" in st.session_state:
+            self.frequency = st.session_state.frequency
+
+    def perform_mining(self) -> None:
+        self.mining_model.create_dependency_graph_with_graphviz(
+            self.threshold, self.frequency
+        )
+
+    def create_empty_model(self, *log_data):
+        return HeuristicMining(*log_data)
+
+    def have_parameters_changed(self) -> bool:
+        return self.mining_model.have_heuristic_parameters_changed()
+
+    def is_correct_model_type(self, model) -> bool:
+        return isinstance(model, HeuristicMining)
+
+    def get_sidebar_values(self) -> dict[str, tuple[int | float, int | float]]:
+        sidebar_values = {
+            "frequency": (1, self.mining_model.get_max_frequency()),
+            "threshold": (0.0, 1.0),
+        }
+
+        return self.mining_model.get_heuristic_sidebar_values()
---
+++ ui/heuristic_miner_ui/heuristic_miner_view.py
@@ -0,0 +1,29 @@
+from ui.base_algorithm_ui.base_algorithm_view import BaseAlgorithmView
+import streamlit as st
+
+
+class HeuristicMinerView(BaseAlgorithmView):
+    def __init__(self):
+        super().__init__()
+
+    def get_page_title(self) -> str:
+        return "Heuristic Mining"
+
+    def render_sidebar(
+        self, sidebar_values: dict[str, tuple[int | float, int | float]]
+    ) -> None:
+        st.slider(
+            label="Minimum Frequency",
+            min_value=sidebar_values["frequency"][0],
+            max_value=sidebar_values["frequency"][1],
+            key="frequency",
+            help="Minimum frequency for displaying edges. Edges with a lower frequency (weight) will be removed.",
+        )
+
+        st.slider(
+            label="Dependency Threshold",
+            min_value=sidebar_values["threshold"][0],
+            max_value=sidebar_values["threshold"][1],
+            key="threshold",
+            help="Minimum dependency for displaying edges. Edges with a lower dependency will be removed.",
+        )
