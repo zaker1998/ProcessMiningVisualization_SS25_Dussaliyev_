fec349308e5155375e1c7952299c3bbe50508433
fabianf00 <fabian.frauenberger@me.com>
2024-05-01 21:29:45 +0200

    move old utils modules

---
+++ io_operations/utils.py
@@ -0,0 +1,72 @@
+import pickle
+import pandas as pd
+import csv
+import os
+import base64
+import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+# TODO: Use docstrings to document the functions not comments
+
+
+# Saves the class object as a pickle file. filename is the full path
+def pickle_save(class_object: object, filename: str) -> None:
+    filePath = filename + ".pickle"
+    with open(filePath, "wb") as file:
+        pickle.dump(class_object, file)
+
+
+# Loads the class object that was saved as a pickle file. path should include filename
+def pickle_load(path: str | UploadedFile) -> object:
+    if isinstance(path, UploadedFile):
+        load_instance = pickle.load(path)
+    else:
+        with open(path, "rb") as file:
+            load_instance = pickle.load(file)
+    return load_instance
+
+
+def detect_delimiter(filePath: str | UploadedFile) -> str:
+    if isinstance(filePath, UploadedFile):
+        dialect = csv.Sniffer().sniff(filePath.readline().decode("utf-8"))
+        filePath.seek(0)
+    else:
+        with open(filePath, "r") as f:
+            dialect = csv.Sniffer().sniff(f.readline())
+
+    delimiter = dialect.delimiter
+    return delimiter
+
+
+def read_csv(filePath: str | UploadedFile, delimiter: str = ",") -> pd.DataFrame:
+    df = pd.read_csv(filePath, delimiter=delimiter)
+    return df
+
+
+def read_excel(filePath: str) -> pd.DataFrame:
+    return pd.read_excel(filePath)
+
+
+def read_file(
+    filePath: str | UploadedFile, delimiter: str = ","
+) -> pd.DataFrame | object:
+    if isinstance(filePath, UploadedFile):
+        file_name = filePath.name
+    else:
+        file_name = filePath
+    if file_name.endswith(".csv"):
+        return read_csv(filePath, delimiter=delimiter)
+    elif file_name.endswith(".pickle"):
+        return pickle_load(filePath)
+    else:
+        # TODO: use a custom io exception
+        raise ValueError("File format not supported")
+
+
+def read_img(filePath: str):
+    png = open(filePath, "rb").read()
+    # https://pmbaumgartner.github.io/streamlitopedia/sizing-and-images.html
+    # https://discuss.streamlit.io/t/how-to-show-local-gif-image/3408/2
+    # Convert the image to a base64 string to be able to display it in the HTML
+    png_base64 = base64.b64encode(png).decode("utf-8")
+    return png_base64
---
+++ transformations/utils.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+def dataframe_to_cases_list(
+    df: pd.DataFrame,
+    timeLabel: str = "timestamp",
+    caseLabel: str = "case",
+    eventLabel: str = "event",
+) -> list[list[str, ...]]:
+    required_columns = [timeLabel, caseLabel, eventLabel]
+    if not all(col in df.columns for col in required_columns):
+        # TODO: Use exceptions instead of raising value errors
+        raise ValueError("Selected columns not found in DataFrame")
+
+    # Sort by timestamp
+    df = df.sort_values(by=[timeLabel])
+
+    result = df.groupby(caseLabel)[eventLabel].apply(list).reset_index(name="events")
+    return result["events"].tolist()
+
+
+def cases_list_to_dict(cases_list: list[list[str, ...]]) -> dict[tuple[str, ...], int]:
+    cases = {}
+    for trace in cases_list:
+        trace_tuple = tuple(trace)
+        cases[trace_tuple] = cases.get(trace_tuple, 0) + 1
+    return cases
+
+
+def dataframe_to_cases_dict(
+    df: pd.DataFrame,
+    timeLabel: str = "timestamp",
+    caseLabel: str = "case",
+    eventLabel: str = "event",
+    **additional_columns
+) -> dict[tuple[str, ...], int]:
+    cases_list = dataframe_to_cases_list(df, timeLabel, caseLabel, eventLabel)
+    return cases_list_to_dict(cases_list)
