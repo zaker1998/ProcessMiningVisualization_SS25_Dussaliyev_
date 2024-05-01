b81225f6196c9fb82d49245996d8c605802e0f75
fabianf00 <fabian.frauenberger@me.com>
2024-05-01 16:01:20 +0200

    remove views directory

--- views/ViewInterface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-import streamlit as st
-
-
-class ViewInterface(ABC):
-
-    @abstractmethod
-    def render(self):
-        raise NotImplementedError("render() method not implemented")
