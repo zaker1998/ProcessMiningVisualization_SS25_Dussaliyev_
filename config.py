84688a933d23e880ee3d2e808c85f1b62438789d
fabianf00 <fabian.frauenberger@me.com>
2024-05-13 20:13:05 +0200

    add section to config.py

--- config.py
+++ config.py
@@ -1,22 +1,25 @@
 from ui.heuristic_miner_ui.heuristic_miner_controller import HeuristicMinerController
 from ui.fuzzy_miner_ui.fuzzy_miner_controller import FuzzyMinerController
 from ui.inductive_miner_ui.inductive_miner_controller import InductiveMinerController
-import streamlit as st
 
-# name : route
+
+# ALGORITHM CONFIGURATIONS
+# ------------------------
+
+# Maps the algorithm names to the route names.
 algorithm_mappings = {
     "Heuristic Mining": "heuristic",
     "Fuzzy Mining": "fuzzy",
     "Inductive Mining": "inductive",
 }
-# route : path
+# Maps the algorithm routes to the paths of the documentation files.
 docs_path_mappings = {
     "heuristic": "docs/algorithms/heuristic_miner.md",
     "fuzzy": "docs/algorithms/fuzzy_miner.md",
     "inductive": "docs/algorithms/inductive_miner.md",
 }
 
-# route : view
+# Maps the algorithm routes to the controllers.
 algorithm_routes = {
     "heuristic": HeuristicMinerController,
     "fuzzy": FuzzyMinerController,
