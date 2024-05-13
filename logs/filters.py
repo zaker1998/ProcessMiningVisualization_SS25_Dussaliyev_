99e2d9d4d417f87741eac692cd6f3f41a6e7d9b7
fabianf00 <fabian.frauenberger@me.com>
2024-05-13 22:16:06 +0200

    add docstring for the log module

--- logs/filters.py
+++ logs/filters.py
@@ -1,4 +1,18 @@
-def filter_events(log, events_to_remove):
+def filter_events(log, events_to_remove: set[str]):
+    """Filter out events from the log.
+
+    Parameters
+    ----------
+    log : dict[tuple[str, ...], int]
+        A dictionary containing the traces and their frequencies in the log.
+    events_to_remove : set[str]
+        A set containing the events that should be removed from the log.
+
+    Returns
+    -------
+    dict[tuple[str, ...], int]
+        A dictionary containing the traces and their frequencies in the log without the events to remove.
+    """
     filtered_log = {}
     for trace, frequency in log.items():
         filtered_trace = [event for event in trace if event not in events_to_remove]
@@ -10,7 +24,21 @@ def filter_events(log, events_to_remove):
     return filtered_log
 
 
-def filter_traces(log, min_frequency):
+def filter_traces(log, min_frequency: int):
+    """Filter out traces from the log that have a frequency below the minimum frequency.
+
+    Parameters
+    ----------
+    log : dict[tuple[str, ...], int]
+        A dictionary containing the traces and their frequencies in the log.
+    min_frequency : int
+        The minimum frequency a trace should have to be kept in the log.
+
+    Returns
+    -------
+    dict[tuple[str, ...], int]
+        A dictionary containing the traces and their frequencies in the log without the traces with a frequency below the minimum frequency.
+    """
     filtered_log = {}
     for trace, frequency in log.items():
         if frequency >= min_frequency:
