437dcc4db9f349eb1082cb69c41f3c1ff303629c
fabianf00 <fabian.frauenberger@me.com>
2024-05-13 22:01:34 +0200

    add inductive miner docstrings

--- mining_algorithms/inductive_mining.py
+++ mining_algorithms/inductive_mining.py
@@ -12,14 +12,34 @@ from logs.filters import filter_events, filter_traces
 
 
 class InductiveMining(BaseMining):
+    """A class to generate a graph from a log using the Inductive Mining algorithm."""
+
     def __init__(self, log):
+        """Constructor for the InductiveMining class.
+
+        Parameters
+        ----------
+        log : dict[tuple[str, ...], int]
+            A dictionary containing the traces and their frequencies in the log.
+        """
         super().__init__(log)
         self.node_sizes = {k: self.calulate_node_size(k) for k in self.events}
         self.activity_threshold = 0.0
         self.traces_threshold = 0.2
         self.filtered_log = None
 
-    def generate_graph(self, activity_threshold, traces_threshold):
+    def generate_graph(self, activity_threshold:float, traces_threshold:float):
+        """Generate a graph from the log using the Inductive Mining algorithm.
+
+        Parameters
+        ----------
+        activity_threshold : float 
+            The activity threshold for the filtering of the log. 
+            All events with a frequency lower than the threshold * max_event_frequency will be removed.
+        traces_threshold : float
+            The traces threshold for the filtering of the log.
+            All traces with a frequency lower than the threshold * max_trace_frequency will be removed.
+        """
         self.activity_threshold = activity_threshold
         self.traces_threshold = traces_threshold
 
@@ -42,6 +62,24 @@ class InductiveMining(BaseMining):
         )
 
     def inductive_mining(self, log):
+        """Generate a process tree from the log using the Inductive Mining algorithm. 
+        This is a recursive function that generates the process tree from the log, 
+        by splitting the log into partitions and generating the tree for each partition.
+        This function uses the base cases, the cut methods and the fallthrough method to generate the tree.
+        If the log is a base case, the corresponding tree is returned. Otherwise, the log is split into partitions using the cut methods.
+        If a cut is found, the tree is generated for each partition. If no cut is found, the fallthrough method is used to generate the tree.
+
+        Parameters
+        ----------
+        log : dict[tuple[str, ...], int]
+            A dictionary containing the traces and their
+
+        Returns
+        -------
+        tuple
+            A tuple representing the process tree. The first element is the operation of the node, the following elements are the children of the node. 
+            The children are either strings representing the events or tuples representing a subtree.
+        """
         if tree := self.base_cases(log):
             return tree
 
@@ -52,7 +90,22 @@ class InductiveMining(BaseMining):
 
         return self.fallthrough(log)
 
-    def base_cases(self, log):
+    def base_cases(self, log) -> str | None:
+        """Check if the log is a base case and return the corresponding tree.
+        The base cases are:
+        - an empty log
+        - a log with a single event
+
+        Parameters
+        ----------
+        log : dict[tuple[str, ...], int]
+            A dictionary containing the traces and their frequencies in the log.
+
+        Returns
+        -------
+        str | None
+            The event in the log if it is a base case, otherwise None.
+        """
         if len(log) > 1:
             return None
 
@@ -65,7 +118,24 @@ class InductiveMining(BaseMining):
 
         return None
 
-    def calulate_cut(self, log):
+    def calulate_cut(self, log) -> tuple | None:
+        """Find a partitioning of the log using the different cut methods.
+        The cut methods are:
+        - exclusive_cut
+        - sequence_cut
+        - parallel_cut
+        - loop_cut
+
+        Parameters
+        ----------
+        log : dict[tuple[str, ...], int]
+            A dictionary containing the traces and their frequencies in the log.
+
+        Returns
+        -------
+        tuple | None
+            A process tree representing the partitioning of the log if a cut was found, otherwise None.
+        """
         dfg = DFG(log)
 
         if partitions := exclusive_cut(dfg):
@@ -80,6 +150,22 @@ class InductiveMining(BaseMining):
         return None
 
     def fallthrough(self, log):
+        """Generate a process tree for the log using a fallthrough method.
+        The following fallthrough method is used:
+        - if there is a empty trace in the log, make an xor split with tau and the inductive mining of the log without the empty trace
+        - if there is a single event in the log and it occures more than once in a trace, make a loop split with the event and tau
+        - if there are multiple events in the log, return a flower model with all the events
+
+        Parameters
+        ----------
+        log : dict[tuple[str, ...], int]
+            A dictionary containing the traces and their frequencies in the log.
+
+        Returns
+        -------
+        tuple
+            A tuple representing the process tree. The first element is the operation of the node, the following elements are the children of the node.
+        """
         log_alphabet = self.get_log_alphabet(log)
 
         # if there is a empty trace in the log
@@ -102,11 +188,37 @@ class InductiveMining(BaseMining):
         # return a flower model with all the events
         return ("loop", "tau", *log_alphabet)
 
-    def get_log_alphabet(self, log):
+    def get_log_alphabet(self, log) -> set[str]:
+        """Get the alphabet of the log. The alphabet is the set of all unique events in the log.
+
+        Parameters
+        ----------
+        log : dict[tuple[str, ...], int]
+            A dictionary containing the traces and their frequencies in the log.
+
+        Returns
+        -------
+        set[str]
+            A set containing all unique events in the log.
+        """
         return set([event for case in log for event in case])
 
-    def get_activity_threshold(self):
+    def get_activity_threshold(self) -> float:
+        """Get the activity threshold used for filtering the log.
+
+        Returns
+        -------
+        float
+            The activity threshold
+        """
         return self.activity_threshold
 
-    def get_traces_threshold(self):
+    def get_traces_threshold(self) -> float:
+        """Get the traces threshold used for filtering the log.
+
+        Returns
+        -------
+        float
+            The traces threshold
+        """
         return self.traces_threshold
