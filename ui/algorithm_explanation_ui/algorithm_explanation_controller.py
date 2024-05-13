f8aa0134145e5a0bdd2ebfbcef1437fbd6ec00ec
fabianf00 <fabian.frauenberger@me.com>
2024-05-13 12:08:17 +0200

    add algorithm explanation docstrings

--- ui/algorithm_explanation_ui/algorithm_explanation_controller.py
+++ ui/algorithm_explanation_ui/algorithm_explanation_controller.py
@@ -6,8 +6,16 @@ from config import docs_path_mappings
 
 
 class AlgorithmExplanationController(BaseController):
+    """Controller for the algorithm explanation page."""
 
     def __init__(self, views=None):
+        """Initializes the controller for the algorithm explanation page.
+
+        Parameters
+        ----------
+        views : List[BaseView] | BaseView, optional
+            The views for the algorithm explanation page. If None is passed, the default view is used, by default None
+        """
         if views is None:
             from ui.algorithm_explanation_ui.algorithm_explanation_view import (
                 AlgorithmExplanationView,
@@ -19,9 +27,19 @@ class AlgorithmExplanationController(BaseController):
         super().__init__(views)
 
     def get_page_title(self) -> str:
+        """Returns the page title. Here a title is not needed as the title is written in the markdown file.
+
+        Returns
+        -------
+        str
+            The page title. In this case, an empty string is returned.
+        """
         return ""
 
     def process_session_state(self):
+        """Processes the session state. Checks if an algorithm has been selected and if the algorithm has documentation.
+        If not, an error message is displayed and the user is navigated back to the home or algorithm page.
+        """
         if "algorithm" not in st.session_state:
             st.session_state.error = "Algorithm not selected"
             to_home()
@@ -33,10 +51,26 @@ class AlgorithmExplanationController(BaseController):
         self.file_path = docs_path_mappings[st.session_state.algorithm]
 
     def read_algorithm_file(self) -> str:
+        """Reads the content of the algorithm markdown file.
+
+        Returns
+        -------
+        str
+            The content of the algorithm markdown file.
+        """
         file_content = self.import_model.read_file(self.file_path)
         return file_content
 
     def run(self, selected_view, index):
+        """Runs the controller. It reads the algorithm file and displays it in the view.
+
+        Parameters
+        ----------
+        selected_view : BaseView
+            The selected view.
+        index : int
+            The index of the selected view in the views list.
+        """
         self.selected_view = selected_view
         selected_view.display_back_button()
         file_content = self.read_algorithm_file()
