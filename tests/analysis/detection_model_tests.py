bf1ce6c13bd9bb483be51213ce699e6322cbb108
fabianf00 <fabian.frauenberger@me.com>
2024-05-29 11:23:34 +0200

    add test cases

--- tests/analysis/detection_model_tests.py
+++ tests/analysis/detection_model_tests.py
@@ -107,6 +107,45 @@ class TestDetectionModel(unittest.TestCase):
         with self.assertRaises(UnsupportedFileTypeException):
             self.detection_model.detect_mime_type("pdf")
 
+    @parameterized.expand(
+        [
+            ("test.pdf", "pdf"),
+            ("test.csv", "csv"),
+            ("test.pickle", "pickle"),
+            ("test.pkl", "pickle"),
+            ("test.xlsx", "excel"),
+            ("test.xls", "excel"),
+            ("test.xlsm", "excel"),
+            ("test.odt", "excel"),
+        ]
+    )
+    def test_detection_model_with_custom_file_mappings_return_correct_types(
+        self, file_path, expected_file_type
+    ):
+        custom_file_mapping = file_types_mapping | {
+            "pdf": [".pdf"],
+            "excel": [".xlsx", ".xls", ".xlsm", "odt"],
+        }
+        detection_model = DetectionModel(
+            file_type_mappings=custom_file_mapping,
+            mime_type_mappings=graph_export_mime_types,
+        )
+        self.assertEqual(
+            detection_model.detect_file_type(file_path), expected_file_type
+        )
+
+    def test_detection_model_with_custom_graph_export_mime_types_return_correct_types(
+        self,
+    ):
+        custom_graph_export_mime_types = graph_export_mime_types | {
+            "pdf": "application/pdf",
+        }
+        detection_model = DetectionModel(
+            file_type_mappings=file_types_mapping,
+            mime_type_mappings=custom_graph_export_mime_types,
+        )
+        self.assertEqual(detection_model.detect_mime_type("pdf"), "application/pdf")
+
 
 if __name__ == "__main__":
     unittest.main()
