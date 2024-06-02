cd489480ed2121b14ad6a9deca3fc0416ad533f9
fabianf00 <fabian.frauenberger@me.com>
2024-06-02 22:55:47 +0200

    add tests for df transformation

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,7 @@ from .mining_algorithms import *
 from .graphs import *
 from .logs import *
 from .analysis import *
+from .transformations import *
 
 if __name__ == "__main__":
     unittest.main()
