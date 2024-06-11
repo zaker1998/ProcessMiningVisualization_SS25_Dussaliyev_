af495193c45264b35babbbaaa6cf15ba4cdf24c6
fabianf00 <fabian.frauenberger@me.com>
2024-06-11 22:02:12 +0200

    change message format

--- logger.py
+++ logger.py
@@ -5,7 +5,7 @@ logging_config = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "simple": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}
+        "simple": {"format": "[%(asctime)s] %(name)s - %(levelname)s : %(message)s"}
     },
     "handlers": {
         "console": {
