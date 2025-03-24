2168f73580f6a002a8e3706e4834435d7caf2a03
Marat Dussaliyev <a01642049@unet.univie.ac.at>
2025-03-24 17:05:36 +0100

    InvalidTypeException added in io_exceptions.py

--- exceptions/io_exceptions.py
+++ exceptions/io_exceptions.py
@@ -7,16 +7,24 @@ class InvalidColumnNameException(Exception):
 
 
 class UnsupportedFileTypeException(Exception):
-    """Exception raised when the file type is not supported."""
+    """Exception raised when a file type is not supported."""
 
-    def __init__(self, file_type):
-        self.message = f"Unsupported file type '{file_type}'"
+    def __init__(self, file_type: str):
+        self.message = f"File type '{file_type}' is not supported."
         super().__init__(self.message)
 
 
 class NotImplementedFileTypeException(Exception):
-    """Exception raised when the file type is supported but not implemented."""
+    """Exception raised when a file type is not implemented."""
 
-    def __init__(self, file_type):
-        self.message = f"File type '{file_type}' not implemented"
+    def __init__(self, file_type: str):
+        self.message = f"File type '{file_type}' is not implemented."
+        super().__init__(self.message)
+
+
+class InvalidTypeException(Exception):
+    """Exception raised when a type is invalid."""
+
+    def __init__(self, expected_type: str, received_type: type):
+        self.message = f"Expected type '{expected_type}', but received '{received_type}'."
         super().__init__(self.message)
